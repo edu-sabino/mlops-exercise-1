4eb471fb4c15fb5dceed92471b290ccd2d03abc2
Eduardo Sabino <68930625+edu-sabino@users.noreply.github.com>
2023-08-03 10:17:09 -0300

    Update train.py
    Challenge 0 activities - enabling auto logging and defining the function to split the data into train and test.

--- src/model/train.py
+++ src/model/train.py
@@ -5,14 +5,17 @@ import glob
 import os
 
 import pandas as pd
+import numpy as np
+import mlflow
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
 
 # define functions
 def main(args):
     # TO DO: enable autologging
-
+    mlflow.autolog()
 
     # read data
     df = get_csvs_df(args.training_data)
@@ -34,12 +37,17 @@ def get_csvs_df(path):
 
 
 # TO DO: add function to split data
-
+def split_data(df):
+    # dividing the dataset into features and target columns
+    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+    # splitting the dataset in a 0.3 ratio for test/train
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+    # return the datasets after splitting
+    return X_train, X_test, y_train, y_test
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
     LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
-
 
 def parse_args():
     # setup arg parser
